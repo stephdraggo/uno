0ed224ebf8834875b11755212c60570b51c4b0ad
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2021-01-02 22:06:42 +1100

    some start

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    #region Variables
+    public CardManager manager;
+    private CardColour colour;
+    private CardValue cValue;
+    #endregion
+    #region Properties
+    public CardColour Colour 
+    { 
+        get => colour;
+        set
+        {
+            if (!manager.cardsInPlay.Contains(this))
+            {
+                colour = value;
+            }
+            else
+            {
+                Debug.LogError("colour already set for this card");
+            }
+        }
+    }
+    public CardValue Value 
+    { 
+        get => cValue;
+        set
+        {
+            if (!manager.cardsInPlay.Contains(this))
+            {
+                cValue = value;
+            }
+            else
+            {
+                Debug.LogError("value already set for this card");
+            }
+        }
+    }
+    #endregion
+    void Start()
+    {
+        manager = FindObjectOfType<CardManager>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
